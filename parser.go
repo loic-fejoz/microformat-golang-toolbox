93b507aa40daaef087a94fe46f5d272ca36a84c2
Loïc Fejoz <loic@fejoz.net>
2014-11-10 16:55:35 +0100

    Implement parsing of simple h-card of the form <a class='h-card' href='http://www.example.com/Someone'>Someone</>.

--- parser.go
+++ parser.go
@@ -2,25 +2,61 @@ package microformat2
 
 import (
 	"golang.org/x/net/html"
+	"bytes"
 	"strings"
 )
 
+func accText(a_node *html.Node, buff *bytes.Buffer) {
+	if a_node.Type == html.TextNode {
+		buff.Write([]byte(a_node.Data))
+	}
+	// Visit children
+	for c := a_node.FirstChild; c != nil; c = c.NextSibling {
+		accText(c, buff)
+	}
+}
+
+func getText(a_node *html.Node) string {
+	var b bytes.Buffer
+	accText(a_node, &b)
+	return b.String()
+}
+
 type Element struct {
-	types []string
-	properties map[string]interface{}
+	Types      []string
+	Properties map[string][]interface{}
+	isHCard bool
+}
+
+func NewElement(types []string) *Element {
+	result := &Element{Types: types, Properties: make(map[string][]interface{})}
+	for _, a_type := range(types) {
+		if a_type == "h-card" {
+			result.isHCard = true
+		}
+	}
+	return result
+}
+
+func AppendProperty(elt *Element, propName string, propVal interface{}) {
+	props := elt.Properties[propName]
+	if props == nil {
+		props = make([]interface{}, 0, 3)
+	}
+	elt.Properties[propName] = append(props, propVal)
 }
 
 type Result struct {
-	items []*Element
-	rels map[string]string
-	alternates []interface{}
+	Items      []*Element
+	Rels       map[string]string
+	Alternates []interface{}
 }
 
 func New() (res *Result) {
 	res = &Result{}
-	res.items = make([]*Element, 0, 5)
-	res.rels = map[string]string{}
-	res.alternates = make([]interface{}, 0, 5)
+	res.Items = make([]*Element, 0, 5)
+	res.Rels = map[string]string{}
+	res.Alternates = make([]interface{}, 0, 5)
 	return
 }
 
@@ -33,10 +69,20 @@ func getClasses(a_node *html.Node) []string {
 	return make([]string, 0, 0)
 }
 
-func filterRootEntry(classes []string) []string {
-	out := make([]string, 0 , len(classes))
+func filterRootClass(classes []string) []string {
+	out := make([]string, 0, len(classes))
 	for _, a_class := range classes {
 		if strings.HasPrefix(a_class, "h-") {
+			out = append(out, a_class)
+		}
+	}
+	return out
+}
+
+func filterPropertyClass(classes []string) []string {
+	out := make([]string, 0, len(classes))
+	for _, a_class := range classes {
+		if strings.HasPrefix(a_class, "p-") || strings.HasPrefix(a_class, "u-") || strings.HasPrefix(a_class, "dt-") || strings.HasPrefix(a_class, "e-") {
 			out = append(out, a_class)
 		}
 	}
@@ -46,7 +92,7 @@ func filterRootEntry(classes []string) []string {
 func isRoot(classes []string) bool {
 	for _, a_class := range classes {
 		if strings.HasPrefix(a_class, "h-") {
-			return true;
+			return true
 		}
 	}
 	return false
@@ -57,10 +103,20 @@ func AccParse(a_node *html.Node, result *Result, root *Element) {
 	if a_node.Type == html.ElementNode {
 		all_classes := getClasses(a_node)
 		if isRoot(all_classes) {
-			types := filterRootEntry(all_classes)
-			current_root = &Element{types: types}
+			types := filterRootClass(all_classes)
+			current_root = NewElement(types)
 			if root == nil {
-				result.items = append(result.items, current_root)
+				result.Items = append(result.Items, current_root)
+			}
+
+			// <a class="h-card..." href="...">...</a>
+			if ((root == current_root || root == nil) && a_node.Data == "a" && current_root.isHCard) {
+				for _, a := range a_node.Attr {
+					if a.Key == "href" {
+						AppendProperty(current_root, "url", a.Val)
+					}
+				}
+				AppendProperty(current_root, "name", getText(a_node))
 			}
 		}
 	}
--- parser_test.go
+++ parser_test.go
@@ -20,23 +20,23 @@ func TestEmptyParagraph(t *testing.T) {
 		t.Fatal("No data returned.")
 		return
 	}
-	if r.items == nil {
+	if r.Items == nil {
 		t.Fatal("items must be not nil.")
 	}
-	if len(r.items) != 0 {
-		t.Error("Expected no items, Actual ", len(r.items))
+	if len(r.Items) != 0 {
+		t.Error("Expected no items, Actual ", len(r.Items))
 	}
-	if r.rels == nil {
+	if r.Rels == nil {
 		t.Fatal("rels must be not nil.")
 	}
-	if len(r.rels) != 0 {
-		t.Error("Expected no rels, Actual ", len(r.rels))
+	if len(r.Rels) != 0 {
+		t.Error("Expected no rels, Actual ", len(r.Rels))
 	}
-	if r.alternates == nil {
+	if r.Alternates == nil {
 		t.Fatal("alternates must be not nil.")
 	}
-	if len(r.alternates) != 0 {
-		t.Error("Expected no alternates, Actual ", len(r.alternates))
+	if len(r.Alternates) != 0 {
+		t.Error("Expected no alternates, Actual ", len(r.Alternates))
 	}
 }
 
@@ -53,7 +53,37 @@ func TestSimpleHCard(t *testing.T) {
 	if r == nil {
 		t.Fatal("No data returned.")
 	}
-	if len(r.items) != 1 {
-		t.Error("Expected 1 items, Actual ", len(r.items))
+	if len(r.Items) != 1 {
+		t.Error("Expected 1 items, Actual ", len(r.Items))
+	}
+	h_card := r.Items[0]
+	if h_card.Types == nil {
+		t.Fatal("types property must not be null.")
+	}
+	if len(h_card.Types) != 1 {
+		t.Fatal("Expected one type h-card, Actual ", len(h_card.Types), ": ", h_card.Types)
+	}
+	if h_card.Types[0] != "h-card" {
+		t.Fatal("Expected type 'h-card', Actual '", h_card.Types[0], "'")
+	}
+	if h_card.Properties == nil {
+		t.Fatal("properties property must not be null.")
+	}
+	if len(h_card.Properties) != 2 {
+		t.Fatal("Expected two properties, Actual ", len(h_card.Properties), ": ", h_card.Properties)
+	}
+	urls := h_card.Properties["url"]
+	if urls == nil {
+		t.Fatal("url must not be null")
+	}
+	if urls[0] != "http://www.example.com/Someone" {
+		t.Fatal("url must be 'http://www.example.com/Someone'")
+	}
+	names := h_card.Properties["name"]
+	if names == nil {
+		t.Fatal("name must not be null")
+	}
+	if names[0] != "Someone" {
+		t.Fatal("Expected name 'Someone', Actual '", names[0], "'")
 	}
 }
